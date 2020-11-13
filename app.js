fdbd12b5ab3ecc64b1f8d4330315f1bdd89db5d8
munna <munnaislampapu@gmail.com>
2020-11-13 22:24:16 +0600

    all file added

---
+++ app.js
@@ -0,0 +1,48 @@
+let picture = document.querySelector('.pictures');
+let rightClick = document.querySelector('.right-arrow');
+let leftClick = document.querySelector('.left-arrow');
+
+
+let picArray = ['pic1.png', 'pic2.png', 'pic3.png', 'pic4.png'];
+
+let perClickResponse = 0;
+console.log(`picArray length = ${picArray.length} and perClickResponse = ${perClickResponse}`);
+
+
+
+
+//click event for right click
+rightClick.addEventListener('click', rightClickEvent);
+
+
+function rightClickEvent(){
+ console.log('right click event work');
+ perClickResponse += 1;
+ if(perClickResponse <= (picArray.length-1)){
+ picture.style.background = `url(/pictures/${picArray[perClickResponse]})`;
+ console.log(perClickResponse);
+ } else {
+  perClickResponse *= 0;
+  picture.style.background = `url(/pictures/${picArray[perClickResponse]})`;
+  console.log(perClickResponse);
+ }
+ 
+}
+
+
+
+//click event for left click
+leftClick.addEventListener('click', leftClickEvent);
+
+function leftClickEvent(){
+ console.log('left click event work');
+ perClickResponse -= 1;
+ if(perClickResponse <= (picArray.length-1) && perClickResponse != -1){
+  picture.style.background = `url(/pictures/${picArray[perClickResponse]})`;
+  console.log(perClickResponse);
+  } else {
+   perClickResponse = (picArray.length-1);
+   picture.style.background = `url(/pictures/${picArray[Math.abs(perClickResponse)]})`;
+   console.log(perClickResponse);
+  }
+}
